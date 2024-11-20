b146abee574de0d9cd69c27aa87952b408c916b1
RomanVII-2022 <vicmkinyua77@gmail.com>
2024-11-20 15:17:55 +0300

    feat: application dockerazation

--- backend/personalityAPI/config/config.js
+++ backend/personalityAPI/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: "127.0.0.1",
+    host: "db",
     dialect: "postgres",
   },
   test: {
